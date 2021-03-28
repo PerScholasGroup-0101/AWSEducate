1deb225b5030d2555678cbaa0b480717b4599809
geewillits <64938876+geewillits@users.noreply.github.com>
2021-03-27 21:11:38 -0400

    Create skills-pathway.py
    Here is s simple dictionary that we can tinker with to produce the skills that our user chooses.

---
+++ skills-pathway.py
@@ -0,0 +1,16 @@
+skill_path = {'1': 'Cloud Computing 101',
+    '2': 'Application Developer',
+    '3': 'Cloud Support Associate',
+    '4': 'Cloud Support Engineer',
+    '5': 'Cybersecurity Specialist',
+    '6': 'Data Integration Specialist',
+    '7': 'Data Scientist',
+    '8': 'DevOps Engineer',
+    '9': 'Machine Learning Scientist',
+    '10': 'Software Development Engineer',
+    '11': 'Solutions Architect',
+    '12': 'Web Development Engineer'
+    }
+
+skill = input('What skill 1-12 are you most interested in? ')
+print(skill_path[skill])
